5fa94ee980e128939ede07193ae30b67201319c7
vamshichi <chvamshi03@example.com>
2024-11-15 16:26:46 +0530

    added animaction for herosection

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ import ExhibitionDetails from "@/app/components/ExhibitionDetails"
 import bgImage from "@/app/images/Coverz.jpg"
 import logo from "@/app/images/white logo.png"
 import ExhibitorForm from './components/exhibitor'
-import FacebookComponent from './components/FacebookAds'
+// import FacebookComponent from './components/FacebookAds'
+import FacebookAds from './components/FacebookAds'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 overflow-hidden">
-      <FacebookComponent />
+      <FacebookAds />
       {/* Background Image */}
       <div className="fixed inset-0">
         <Image
