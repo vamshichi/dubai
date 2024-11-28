2c72a542356651d251d69653fff14924aac61556
vamshichi <chvamshi03@example.com>
2024-11-28 18:26:27 +0530

    added ips ga adds

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,14 +2,19 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+// Use a global type for `prisma` to avoid `any`.
+declare global {
+  // This ensures `global.prisma` has the correct type in Node.js
+  var prisma: PrismaClient | undefined;
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-  prisma = (global as any).prisma;
+  prisma = global.prisma;
 }
 
 export default prisma;
-
