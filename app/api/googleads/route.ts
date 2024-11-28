c8558ea47fab2c670ea7bdbf48baf50d161ae3a3
vamshichi <chvamshi03@example.com>
2024-11-28 18:26:56 +0530

    added ips ga adds

--- app/api/googleads/route.ts
+++ app/api/googleads/route.ts
@@ -1,6 +1,6 @@
 // app/api/registration/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import {prisma} from '@/lib/prisma'; // Adjust path if necessary
+import prisma from '@/lib/prisma'; // Adjust path if necessary
 
 export async function POST(req: NextRequest) {
   try {
