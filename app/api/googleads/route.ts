b0c12ef787e882e5210233347139769df080eb9b
vamshichi <chvamshi03@example.com>
2024-11-28 13:22:24 +0530

    added ips ga adds

---
+++ app/api/googleads/route.ts
@@ -0,0 +1,26 @@
+// app/api/registration/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prismaClient'; // Adjust path if necessary
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, phone, preferredCity, preferredBudget } = await req.json();
+
+    // Save to MongoDB using Prisma
+    const registrationga = await prisma.googleads.create({
+      data: {
+        name,
+        email,
+        phone,
+        preferredCity,
+        preferredBudget,
+      },
+    });
+
+    const response = NextResponse.json({ message: "Registration successful", registrationga }, { status: 201 });
+    response.headers.set('Cache-Control', 'no-store'); // Prevent caching
+    return response;
+  } catch (error) {
+    return NextResponse.json({ message: "Failed to register", error: error instanceof Error ? error.message : "An unknown error occurred" }, { status: 500 });
+  }
+}
---
+++ app/googleadmin/page.tsx
@@ -0,0 +1,27 @@
+// app/admin/page.tsx
+import { PrismaClient } from "@prisma/client";
+import DateRangePicker from '@/app/admin/DateRangePicker';
+import DownloadButton from '@/app/admin/DownloadButton';
+import RegistrationTable from '@/app/admin/RegistrationTable';
+
+const prisma = new PrismaClient();
+
+export default async function AdminPage() {
+  const registrations = await prisma.googleads.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Admin Dashboard - All Registrations</h1>
+      <div className="mb-4">
+        <div className="pb-8">
+          <DateRangePicker />
+        </div>
+        <DownloadButton registrations={registrations} />
+      </div>
+      <RegistrationTable registrations={registrations} />
+    </div>
+  );
+}
+
