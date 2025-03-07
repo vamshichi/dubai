d3aec2dd4a693430df7baf4d57bea8d64b7d9dc2
Sandeep1816 <sandeepavala2@gmail.com>
2025-03-07 13:02:46 +0530

    Updated error changed

--- app/components/FacebookPixel.tsx
+++ app/components/FacebookPixel.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Script from "next/script";
-import Image from "next/image";
 
 const FacebookPixel = () => {
   return (
@@ -9,19 +8,19 @@ const FacebookPixel = () => {
       <Script id="facebook-pixel" strategy="afterInteractive">
         {`
           !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1364293904985120');
-fbq('track', 'PageView');
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1364293904985120');
+          fbq('track', 'PageView');
         `}
       </Script>
       <noscript>
-        <Image
+        <img
           height="1"
           width="1"
           style={{ display: "none" }}
